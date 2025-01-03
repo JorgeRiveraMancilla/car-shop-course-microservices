07a2c0d37916c156f19dae677122206ae6440018
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-03 20:53:46 -0300

    feat: implement get auction by id endpoint

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -25,5 +25,19 @@ namespace AuctionService.Controllers
 
             return Ok(auctions);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+        {
+            var auction = await _dataContext
+                .Auctions.Include(x => x.Item)
+                .ProjectTo<AuctionDto>(_mapper.ConfigurationProvider)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (auction == null)
+                return NotFound();
+
+            return Ok(auction);
+        }
     }
 }
