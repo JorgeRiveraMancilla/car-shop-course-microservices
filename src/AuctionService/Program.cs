276503d4ccc826b3f8f83ff2e309841e864dc0a5
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-03 19:49:29 -0300

    feat: configure database connection

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,10 +1,29 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
 app.UseAuthorization();
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
