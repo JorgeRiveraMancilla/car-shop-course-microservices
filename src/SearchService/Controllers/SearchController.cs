88f16ec3b06af1848ef140fb6f41b4aa9531ac4a
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-03 21:32:43 -0300

    feat: create search service

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchService.Controllers
+{
+    public class SearchController
+    {
+        
+    }
+}
