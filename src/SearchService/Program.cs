bdbab08a89a919628aa9c950dacd4be680637911
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-03 21:46:52 -0300

    feat: configure http client and database

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,10 +1,31 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
 app.UseAuthorization();
 app.MapControllers();
 
+try
+{
+    await DatabaseInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
 app.Run();
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() =>
+    HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
