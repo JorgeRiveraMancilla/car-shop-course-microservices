f67ca114fe62473bd46d645e9ecc3185d10fe0c8
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-05 14:42:45 -0300

    feat: add identity service from duende-software template

---
+++ src/IdentityService/Pages/Home/Error/Index.cshtml.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Home.Error
+{
+    [AllowAnonymous]
+    [SecurityHeaders]
+    public class Index(IIdentityServerInteractionService interaction, IWebHostEnvironment environment) : PageModel
+    {
+        private readonly IIdentityServerInteractionService _interaction = interaction;
+        private readonly IWebHostEnvironment _environment = environment;
+
+        public ViewModel View { get; set; } = new();
+
+        public async Task OnGet(string? errorId)
+        {
+            // retrieve error details from identityserver
+            var message = await _interaction.GetErrorContextAsync(errorId);
+            if (message != null)
+            {
+                View.Error = message;
+
+                if (!_environment.IsDevelopment())
+                {
+                    // only show in development
+                    message.ErrorDescription = null;
+                }
+            }
+        }
+    }
+}
